f03903773234023d2da69d936a25e39f0783aae9
Roneyroney21 <148005561+Roneyroney21@users.noreply.github.com>
2023-10-16 01:36:06 +0300

    task

---
+++ loop.py
@@ -0,0 +1,155 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "6950e58c",
+   "metadata": {},
+   "source": [
+    "# data science tools and ecosystem"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "8c431ec5",
+   "metadata": {},
+   "source": [
+    "in this notebook, data science tools and ecosystem are summarized"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "e167ee1a",
+   "metadata": {},
+   "source": [
+    "some of the popular languages that data scientists use are:\n",
+    "1. python\n",
+    "2. R\n",
+    "3. c++"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "aa55dfb6",
+   "metadata": {},
+   "source": [
+    "some of the commonly used libraries used by data scientists include:\n",
+    "1. numpy\n",
+    "2. ggplot \n",
+    "3. pytorch\n",
+    "4. matpoltlib\n",
+    "5. pandas"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "f30f60f2",
+   "metadata": {},
+   "source": [
+    "|data sience tools|\n",
+    "|--------------|\n",
+    "|SAS|\n",
+    "|mysql|\n",
+    "|hadoop|"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a6856639",
+   "metadata": {},
+   "source": [
+    "### below are a few exsamples of evaluating arithmetic expressions in python"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "a6784090",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "17"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "#this a simple arithmetic to mutply then add integers\n",
+    "(3*4)+5"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "074ecee7",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "3.3333333333333335"
+      ]
+     },
+     "execution_count": 4,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "#this will convert 200min to hours by diving by 60\n",
+    "200/60"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b827ca91",
+   "metadata": {},
+   "source": [
+    "## author\n",
+    "\n",
+    "Rayan"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "694586d3",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d951df10",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
